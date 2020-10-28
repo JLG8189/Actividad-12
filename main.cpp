a7cbe4215ce3ac931aa625fcba6e29f8f923a5d0
Jorge Linares <jorge.linares8189@alumnos.udg.mx>
2020-10-28 14:33:36 -0600

    Actividad 12

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "arregloDinamico.h"
+
+using namespace std;
+
+int main()
+{
+    ArregloDinamico arreglo;
+    arreglo.insertar_final("Uno");
+    arreglo.insertar_final("Dos");
+    arreglo.insertar_final("Tres");
+    arreglo.insertar_final("Cuatro");
+    arreglo.insertar_final("Cinco");
+    arreglo.insertar_final("Seis");
+    arreglo.insertar_final("Siete");
+    arreglo.insertar_final("Ocho");
+    arreglo.insertar_inicio("Nueve");
+    arreglo.insertar_inicio("Diez");
+
+    for (size_t i = 0; i < arreglo.size(); i++)
+    {
+        cout << arreglo[i] << " ";
+    }
+}
